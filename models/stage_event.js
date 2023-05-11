985ccda2b4f023d196baf4eb3bb4f54486272fdd
George Leyva <leyvageorge@hotmail.com>
2023-05-10 21:35:55 -0700

    added 5 tables

---
+++ models/stage_event.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Stage_event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Stage_event.init({
+    stage_events_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Stage_event',
+    tableName: 'stage_events',
+    timestamps: false
+  });
+  return Stage_event;
+};
